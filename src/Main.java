5b547ecc3cf61578e028e619e4069710b230d1bb
Gleb Anatolevich <glebagovorov@yandex.ru>
2025-01-24 19:01:31 +0300

    ТЗ-8. Исправлены замечания первого ревью

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,22 @@ import java.time.LocalDateTime;
 
 import static model.Status.*;
 
-public static void main(String[] args) {
+public class Main {
 
-    InMemoryTaskManager taskManager = new InMemoryTaskManager();
-    Task task1 = taskManager.create(new Task(1, "задача 111", NEW, "описание задачи 111",
-            Duration.ofMinutes(120), LocalDateTime.of(2024, 12, 24, 12, 0)));
-    Task task2 = taskManager.create(new Task(2, "задача 111", NEW, "описание задачи 111",
-            Duration.ofMinutes(10), LocalDateTime.of(2024, 12, 25, 15, 0)));
-    Task task3 = taskManager.create(new Task(3, "задача 111", NEW, "описание задачи 111",
-            Duration.ofMinutes(180), LocalDateTime.of(2024, 12, 25, 12, 0)));
+    public static void main(String[] args) {
+        InMemoryTaskManager taskManager = new InMemoryTaskManager();
+        Task task1 = taskManager.create(new Task(1,"задача 111", NEW, "описание задачи 111",
+                Duration.ofMinutes(120), LocalDateTime.of(2024,12,24,12,0)));
+        Task task2 = taskManager.create(new Task(2,"задача 111", NEW, "описание задачи 111",
+                Duration.ofMinutes(10), LocalDateTime.of(2024,12,25,15,0)));
+        Task task3 = taskManager.create(new Task(3,"задача 111", NEW, "описание задачи 111",
+                Duration.ofMinutes(180), LocalDateTime.of(2024,12,25,12,0)));
 
-    task1.setStatus(DONE);
-    task2.setStatus(IN_PROGRESS);
-    taskManager.update(task1);
-    taskManager.update(task2);
+        task1.setStatus(DONE);
+        task2.setStatus(IN_PROGRESS);
+        taskManager.update(task1);
+        taskManager.update(task2);
 
-    System.out.println(taskManager.getPrioritizedTasks());
+        System.out.println(taskManager.getPrioritizedTasks());
+    }
 }
